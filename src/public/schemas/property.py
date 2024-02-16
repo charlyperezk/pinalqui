1a10e66303989b19854e7048724416e032b66603
Carlos Perez Kuper <carlosperezkuper@gmail.com>
2024-02-15 22:49:02 -0300

    Pydantic schemas

--- src/public/schemas/property.py
+++ src/public/schemas/property.py
@@ -6,6 +6,19 @@ from datetime import datetime
 from src.public.schemas.image import ImageRead
 
 # Code
+class PropertyCreate(BaseModel):
+    date: Type[datetime]
+    title: Type[str]
+    address: Type[str]
+    price: Type[float]
+    deposit: Type[float]
+    description: Type[str]
+    status: Type[str]
+    type_id: Type[int]
+    city_id: Type[int]
+    currency_id: Type[int]
+    user: Type[int]
+    
 class PropertyRead(BaseModel):
     id: Type[int]
     date: Type[datetime]
